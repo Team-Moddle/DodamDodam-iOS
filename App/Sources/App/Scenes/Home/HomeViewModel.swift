00be700d6e98b7a213d62bbd14f45b23c7912df8
rlarldud1234 <rlarldud6482@naver.com>
2022-10-05 20:38:48 +0900

    💩🚽

---
+++ App/Sources/App/Scenes/Home/HomeViewModel.swift
@@ -0,0 +1,62 @@
+import Foundation
+
+class HomeViewModel: ObservableObject {
+    @Published var serach: String = ""
+    @Published var list: [CommunityModel] = [
+        .init(
+            imageUrlString: "",
+            title: "우리 아이 이대로 괜찮을까?",
+            location: "서울특별시 강남구",
+            humanCount: 20
+        ),
+        .init(
+            imageUrlString: "",
+            title: "우리 아이 이대로 괜찮을까?",
+            location: "서울특별시 강남구",
+            humanCount: 20
+        ),
+        .init(
+            imageUrlString: "",
+            title: "우리 아이 이대로 괜찮을까?",
+            location: "서울특별시 강남구",
+            humanCount: 20
+        ),
+        .init(
+            imageUrlString: "",
+            title: "우리 아이 이대로 괜찮을까?",
+            location: "서울특별시 강남구",
+            humanCount: 20
+        ),
+        .init(
+            imageUrlString: "",
+            title: "우리 아이 이대로 괜찮을까?",
+            location: "서울특별시 강남구",
+            humanCount: 20
+        ),
+        .init(
+            imageUrlString: "",
+            title: "우리 아이 이대로 괜찮을까?",
+            location: "서울특별시 강남구",
+            humanCount: 20
+        ),
+        .init(
+            imageUrlString: "",
+            title: "우리 아이 이대로 괜찮을까?",
+            location: "서울특별시 강남구",
+            humanCount: 20
+        ),
+        .init(
+            imageUrlString: "",
+            title: "우리 아이 이대로 괜찮을까?",
+            location: "서울특별시 강남구",
+            humanCount: 20
+        ),
+        .init(
+            imageUrlString: "",
+            title: "우리 아이 이대로 괜찮을까?",
+            location: "서울특별시 강남구",
+            humanCount: 20
+        ),
+
+    ]
+}
