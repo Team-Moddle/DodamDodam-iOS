78bff70b3c9075b2edee6eb4f0ff4c73fd839549
baegteun <baegteun@gmail.com>
2022-10-05 14:41:37 +0900

    :art: :: Plist 생성

---
+++ App/Derived/Sources/TuistPlists+DodamDodam.swift
@@ -0,0 +1,32 @@
+// swiftlint:disable all
+// swift-format-ignore-file
+// swiftformat:disable all
+// Generated using tuist — https://github.com/tuist/tuist
+
+import Foundation
+
+// swiftlint:disable superfluous_disable_command
+// swiftlint:disable file_length
+
+// MARK: - Plist Files
+
+// swiftlint:disable identifier_name line_length number_separator type_body_length
+public enum GoogleServiceInfo {
+    public static let apiKey: String = "AIzaSyA396n5BDTDfViyI9F5jETC-w38h9BTRxo"
+    public static let bundleId: String = "org.Moddle.DodamDodam"
+    public static let clientId: String = "172120567525-nre7d0i65fcji9hv5s7qvfpd2lmijgj6.apps.googleusercontent.com"
+    public static let gcmSenderId: String = "172120567525"
+    public static let googleAppId: String = "1:172120567525:ios:97725c43302275d1476e0c"
+    public static let isAdsEnabled: Bool = false
+    public static let isAnalyticsEnabled: Bool = false
+    public static let isAppinviteEnabled: Bool = true
+    public static let isGcmEnabled: Bool = true
+    public static let isSigninEnabled: Bool = true
+    public static let plistVersion: String = "1"
+    public static let projectId: String = "dodamdodam-420b0"
+    public static let reversedClientId: String = "com.googleusercontent.apps.172120567525-nre7d0i65fcji9hv5s7qvfpd2lmijgj6"
+    public static let storageBucket: String = "dodamdodam-420b0.appspot.com"
+}
+// swiftlint:enable identifier_name line_length number_separator type_body_length
+// swiftlint:enable all
+// swiftformat:enable all
