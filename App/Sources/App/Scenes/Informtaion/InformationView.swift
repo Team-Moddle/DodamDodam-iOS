196025fccfac0b23d1e8f4a25d67a12ac53493c4
rlarldud1234 <rlarldud6482@naver.com>
2022-10-06 02:40:57 +0900

    💩🚽

---
+++ App/Sources/App/Scenes/Informtaion/InformationView.swift
@@ -0,0 +1,13 @@
+import SwiftUI
+
+struct InformationView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+struct InformationView_Previews: PreviewProvider {
+    static var previews: some View {
+        InformationView()
+    }
+}
