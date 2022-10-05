0b460f8a822954ee0d2a6d908f7aa77113ee5ab5
rlarldud1234 <rlarldud6482@naver.com>
2022-10-05 20:38:56 +0900

    💩🚽

---
+++ App/Sources/App/Scenes/Home/Search/SearchBar.swift
@@ -0,0 +1,34 @@
+import SwiftUI
+
+struct SearchBar: View {
+    @Binding var search: String
+    let action: () -> Void
+    var body: some View {
+        HStack {
+            TextField(
+                "원하는 키워드를 입력해주세요.",
+                text: $search
+            )
+            .font(.system(size: 14, weight: .light))
+            Button(action: action) {
+                Image(systemName: "magnifyingglass")
+                    .frame(width: 19, height: 19)
+                    .scaledToFit()
+                    .foregroundColor(Color.gray)
+            }
+        }
+        .padding(.horizontal, 8)
+        .frame(height: 46)
+        .background(Color(uiColor: UIColor.lightGray))
+        .cornerRadius(5)
+    }
+}
+
+struct SearchBar_Previews: PreviewProvider {
+    static var previews: some View {
+        SearchBar(
+            search: .constant(""),
+            action: { }
+        )
+    }
+}
