a683c25999324a6a52c5c1af87d7628a9bef399b
rlarldud1234 <rlarldud6482@naver.com>
2022-10-06 13:57:20 +0900

    💩🚽

---
+++ App/Sources/App/Model/ChildModel.swift
@@ -0,0 +1,15 @@
+import Foundation
+
+import SwiftUI
+
+struct ChildModel {
+    let name: String
+    let birthDay: String
+    let personality: String
+    var bool1: Bool
+    let bool2: Bool
+    let bool3: Bool
+    let bool4: Bool
+    let bool5: Bool
+    let guitar: String
+}
