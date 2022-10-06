dfb85cfad72c1fddcb79aba23e926042551a5d96
rlarldud1234 <rlarldud6482@naver.com>
2022-10-06 09:51:36 +0900

    💩🚽

--- App/Sources/App/Scenes/Facility/FacilityViewModel.swift
+++ App/Sources/App/Scenes/Facility/FacilityViewModel.swift
@@ -14,37 +14,37 @@ class FacilityViewModel: ObservableObject {
         ),
         .init(
             id: 1,
-            name: "우리집 병원",
+            name: "우리집 식당",
             address: "인천광역시 미추홀구 동주길67번길 54",
             imageUrlString: "https://cdn.discordapp.com/attachments/1026851673001431091/1027230330891010249/unknown.png",
-            category: .hospital,
+            category: .restaurant,
             commentCount: 2,
             heartCount: 3
         ),
         .init(
             id: 2,
-            name: "우리집 병원",
+            name: "우리집 놀이공원",
             address: "인천광역시 미추홀구 동주길67번길 54",
             imageUrlString: "https://cdn.discordapp.com/attachments/1026851673001431091/1027230330891010249/unknown.png",
-            category: .hospital,
+            category: .playground,
             commentCount: 2,
             heartCount: 3
         ),
         .init(
             id: 3,
-            name: "우리집 병원",
+            name: "우리집 키즈카페",
             address: "인천광역시 미추홀구 동주길67번길 54",
             imageUrlString: "https://cdn.discordapp.com/attachments/1026851673001431091/1027230330891010249/unknown.png",
-            category: .hospital,
+            category: .playground,
             commentCount: 2,
             heartCount: 3
         ),
         .init(
             id: 4,
-            name: "우리집 병원",
+            name: "우리집 옆식당",
             address: "인천광역시 미추홀구 동주길67번길 54",
             imageUrlString: "https://cdn.discordapp.com/attachments/1026851673001431091/1027230330891010249/unknown.png",
-            category: .hospital,
+            category: .restaurant,
             commentCount: 2,
             heartCount: 3
         ),
@@ -58,4 +58,21 @@ class FacilityViewModel: ObservableObject {
             heartCount: 3
         )
     ]
+    @Published var locationList: [LocationModel] = []
+
+    func fetchAppear() {
+        locationList = list
+    }
+
+    func fetchFilterHospital() {
+        locationList = list.filter { $0.category == .hospital }
+    }
+
+    func fetchFilterRestraunt() {
+        locationList = list.filter { $0.category == .restaurant }
+    }
+
+    func fetchFilterPlayground() {
+        locationList = list.filter { $0.category == .playground }
+    }
 }
