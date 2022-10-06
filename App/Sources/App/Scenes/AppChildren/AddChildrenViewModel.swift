76972817be5f549b09adee37c542e5f9c390e4c7
rlarldud1234 <rlarldud6482@naver.com>
2022-10-06 15:45:00 +0900

    💩🚽

--- App/Sources/App/Scenes/AppChildren/AddChildrenViewModel.swift
+++ App/Sources/App/Scenes/AppChildren/AddChildrenViewModel.swift
@@ -10,4 +10,18 @@ class AddChildrenViewModel: ObservableObject {
     @Published var bool4: Bool = false
     @Published var bool5: Bool = false
     @Published var guitar: String = ""
+
+    func setChildren() {
+        ProfileUserDefaults.shared.setChildrenInformation(
+            name: name,
+            birthday: birthday,
+            personality: personality,
+            bool1: bool1,
+            bool2: bool2,
+            bool3: bool3,
+            bool4: bool4,
+            bool5: bool5,
+            guitar: guitar
+        )
+    }
 }
