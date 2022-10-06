3625bc7ed3b3f3e532c404113b1474206ed75fc1
rlarldud1234 <rlarldud6482@naver.com>
2022-10-06 15:45:10 +0900

    💩🚽

--- App/Sources/App/Scenes/EditProfile/EditProfileViewModel.swift
+++ App/Sources/App/Scenes/EditProfile/EditProfileViewModel.swift
@@ -2,8 +2,25 @@ import Combine
 import UIKit
 
 final class EditProfileViewModel: ObservableObject {
-    @Published var profile: ProfileModel = .init(imageUrlString: "", name: "김기영", address: "인천광역시 미추홀구")
+    @Published var profile: ProfileModel = .init(imageUrlString: Data(), name: "", address: "")
     @Published var image: UIImage?
     @Published var name: String = ""
     @Published var address: String = ""
+    let prevProfile: ProfileModel
+
+    init(prevProfile: ProfileModel) {
+        self.prevProfile = prevProfile
+        self.image = UIImage(data: prevProfile.imageUrlString)
+        self.name = prevProfile.name
+        self.address = prevProfile.address
+    }
+
+    func setProfile() {
+        ProfileUserDefaults.shared.setProfile(
+            profile: .init(
+                imageUrlString: (image?.pngData())!,
+                name: name,
+                address: address
+            ))
+    }
 }
