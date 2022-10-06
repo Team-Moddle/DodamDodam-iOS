2802a05204ee65020a4c6f99c1ee81d518f42870
rlarldud1234 <rlarldud6482@naver.com>
2022-10-06 15:45:13 +0900

    💩🚽

--- App/Sources/App/Scenes/Profile/ProfileView.swift
+++ App/Sources/App/Scenes/Profile/ProfileView.swift
@@ -4,40 +4,46 @@ struct ProfileView: View {
     @StateObject var viewModel = ProfileViewModel()
     var body: some View {
         NavigationView {
-            VStack(spacing: 0) {
-                Spacer()
-                    .frame(height: 19)
-                ProfileInformationView(profile: viewModel.profile)
-                    .padding(.horizontal, 20)
-                    .padding(.bottom, 21)
-                HStack {
-                    Text("자녀 정보")
-                        .font(.system(size: 15, weight: .medium))
-                        .foregroundColor(Color("6B6B6B"))
+            ScrollView {
+                VStack(spacing: 0) {
                     Spacer()
-                    NavigationLink {
-                        AddChildrenView()
-                    } label: {
-                        Image(systemName: "plus")
-                            .resizable()
-                            .frame(width: 15, height: 15)
-                            .foregroundColor(.black)
+                        .frame(height: 19)
+                    ProfileInformationView(profile: viewModel.profile)
+                        .padding(.horizontal, 20)
+                        .padding(.bottom, 21)
+                    HStack {
+                        Text("자녀 정보")
+                            .font(.system(size: 15, weight: .medium))
+                            .foregroundColor(Color("6B6B6B"))
+                        Spacer()
+                        NavigationLink {
+                            AddChildrenView()
+                        } label: {
+                            Image(systemName: "plus")
+                                .resizable()
+                                .frame(width: 15, height: 15)
+                                .foregroundColor(.black)
+                        }
                     }
+                    .padding(.horizontal, 20)
+                    Spacer()
+                        .frame(height: 13)
+                    ForEach(viewModel.child, id: \.name) {
+                        ProfileCell(child: $0)
+                            .padding(.bottom, 13)
+                            .listRowInsets(EdgeInsets())
+                            .listRowSeparator(.hidden)
+                    }
+                    .padding(.horizontal, 20)
+                    .listStyle(.inset)
                 }
-                .padding(.horizontal, 20)
-                Spacer()
-                    .frame(height: 13)
-                List(viewModel.child, id: \.name) {
-                    ProfileCell(child: $0)
-                        .padding(.bottom, 13)
-                        .listRowInsets(EdgeInsets())
-                        .listRowSeparator(.hidden)
+                .onAppear {
+                    viewModel.fetchChildren()
+                    viewModel.fetchProfile()
                 }
-                .padding(.horizontal, 20)
-                .listStyle(.inset)
+                .navigationTitle("내정보")
+                .navigationBarTitleDisplayMode(.large)
             }
-            .navigationTitle("내정보")
-            .navigationBarTitleDisplayMode(.large)
         }
     }
 }
